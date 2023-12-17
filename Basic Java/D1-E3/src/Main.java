991ffca453a017a47620954ee14a6c51562982bb
ZManak <javiefe.dev@gmail.com>
2023-12-17 18:48:39 +0100

    D1-E3

---
+++ Basic Java/D1-E3/src/Main.java
@@ -0,0 +1,9 @@
+/*Write a program that allows you to calculate the area of a triangle and then display the result on the screen. Remember that the formula to calculate this is: (base * height) / 2 .*/
+public class Main {
+    public static void main(String[] args) {
+        int base = 10;
+        int height = 5;
+        int area = (base * height) / 2;
+        System.out.println("The area of a triangle with base " + base + " and height " + height + " is " + area);
+    }
+}
