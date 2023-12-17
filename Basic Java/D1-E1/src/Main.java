eb1d1ab35a7b9494aae2ae38f966eeba9d6d3e19
ZManak <javiefe.dev@gmail.com>
2023-12-17 18:25:45 +0100

    D1-E1

---
+++ Basic Java/D1-E1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome Captain Morgan!");
+        String name = "Captain Falcon";
+        System.out.println("Welcome " + name + "!");
+    }
+}
